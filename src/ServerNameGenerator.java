557bd75cad7f368560432a9eccaf1357c25878c3
Cory Musick <cory.musick37@gmail.com>
2019-03-27 10:53:19 -0500

    servername generator complete

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,20 @@
+public class ServerNameGenerator {
+    public static String[] adjectives = {"Legendary", "big","cool","awesome","mediocre","sad", "disappointing","Super","Amazing","Mind-Blowing"};
+    public static String[] nouns = {"toothbrush","cat","zebra","puma","ocelot","cow", "moose","mouse","flamingo","lion"};
+
+    public static String generator(String[] array){
+      return array[(int) (Math.random()* 9)];
+    }
+
+    public static String createName(String[] array1, String[] array2){
+        String firstWord = generator(array1);
+        String secondWord = generator(array2);
+
+        return firstWord + "-" + secondWord;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Here is your server name:");
+        System.out.println(createName(adjectives,nouns));
+    }
+}
